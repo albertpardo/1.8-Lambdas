e2c23d29d9ba8821d039aa43ae2356d6782a6614
apm <apm@mail.com>
2025-11-24 15:23:51 +0100

    [T8L1E6] Done

--- src/level1ex6/Main.java
+++ src/level1ex6/Main.java
@@ -1,7 +1,47 @@
 package level1ex6;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
-    public static void  main(String[] args){
-        
+    private static List<Object> createObjectsList(){
+        List<Object> list = new ArrayList<>();
+
+        list.add("Word");
+        list.add(434.01);
+        list.add("Octopussy");
+        list.add(1324164674);
+        list.add("Abracadabra");
+        list.add("A");
+        list.add("Input");
+        return list;
     }
+    private static List<String> sortListByIncrementalLength(List<Object> objectsList) {
+        List<String> sortedList = objectsList.stream()
+                .map(Object::toString)
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+        return sortedList;
+    }
+
+    private static void printLists(List<Object> objectsList, List<String> objectsListByIncrementalLength) {
+        System.out.println("-- Original List :");
+        objectsList.forEach(System.out::println);
+        System.out.println("-- Sorted List By Incremental Length :");
+        objectsListByIncrementalLength.forEach(System.out::println);
+    }
+
+    public static void  main(String[] args){
+        List<Object> objectsList;
+        List<String> objectsListByIncrementalLength;
+
+        objectsList = createObjectsList();
+        objectsListByIncrementalLength = sortListByIncrementalLength(objectsList);
+        printLists(objectsList, objectsListByIncrementalLength);
+    }
+
+
+
+
 }
