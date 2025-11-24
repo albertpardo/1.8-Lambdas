5072b6e5e2fae58bc51c3a20c4c773a7b1553ef6
apm <apm@mail.com>
2025-11-24 16:31:49 +0100

    [T8L1E8] workind on solution

--- src/level1ex8/Main.java
+++ src/level1ex8/Main.java
@@ -2,6 +2,6 @@ package level1ex8;
 
 public class Main {
     public static void main(String[] args){
-        
+
     }
 }
---
+++ src/level1ex8/interfaces/reverseFunctionalInterface.java
@@ -0,0 +1,6 @@
+package level1ex8.interfaces;
+
+@FunctionalInterface
+public interface reverseFunctionalInterface {
+    public String reverse(String inputString);
+}
