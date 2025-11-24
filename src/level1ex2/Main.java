69246f2005d31127e4a60cce8fb57ca559472cbf
apm <apm@mail.com>
2025-11-24 11:41:37 +0100

    [T8L1E2] start

---
+++ src/level1ex2/Main.java
@@ -0,0 +1,30 @@
+package level1ex2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static List<String> filterByCharO(List<String> list){
+        return list.stream().filter(string -> (string.contains("o") || string.contains("O"))).toList();
+    }
+
+    private static List<String> createWordList(){
+        List<String> list = new ArrayList<>();
+
+        list.add("Hola");
+        list.add("Peppa");
+        list.add("Car");
+        list.add("Octopussy");
+        list.add("Music");
+        list.add("Octave");
+        return list;
+    }
+
+    public static void main(String[] args){
+
+        List<String> myStringList;
+
+        myStringList = createWordList();
+        System.out.println(filterByCharO(myStringList));
+    }
+}
