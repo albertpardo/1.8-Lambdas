12f9579ad38ef1cf7dbef6262dc4419cece26f9e
apm <apm@mail.com>
2025-11-24 18:53:16 +0100

    [T8L2E3] Done

--- src/level2ex3/Interfaces/MathOperationFunctionalInterface.java
+++ src/level2ex3/Interfaces/MathOperationFunctionalInterface.java
@@ -2,5 +2,5 @@ package level2ex3.Interfaces;
 
 @FunctionalInterface
 public interface MathOperationFunctionalInterface {
-    float operation();
+    float operation(float numA, float numB);
 }
--- src/level2ex3/Main.java
+++ src/level2ex3/Main.java
@@ -1,7 +1,17 @@
 package level2ex3;
 
+import level2ex3.Interfaces.MathOperationFunctionalInterface;
+
 public class Main {
     public static void main(String[] args){
-        
+        MathOperationFunctionalInterface add = (a, b) -> a + b;
+        MathOperationFunctionalInterface substraction = (a , b) -> a - b;
+        MathOperationFunctionalInterface product = (a, b) -> a * b;
+        MathOperationFunctionalInterface division = (a, b) -> a / b;
+
+        System.out.println("Add operation : 40 + 1.1 = " + add.operation( 40.0f , 1.1f));
+        System.out.println("Substraction operation : 25.4 - 5.1 = " + substraction.operation(25.4f, 5.1f));
+        System.out.println("Product operation : 2.2 * 10.0 = " + product.operation(2.2f, 10.0f));
+        System.out.println("Division operation : 1000.0 / 20.0 = " + division.operation(1000.0f, 20.0f));
     }
 }
