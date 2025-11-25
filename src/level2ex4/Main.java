26cf274b6e35137ba48d8ed5de90f19c835439b2
apm <apm@mail.com>
2025-11-25 10:09:02 +0100

    [T8L2E4] workin on it

--- src/level2ex4/Main.java
+++ src/level2ex4/Main.java
@@ -1,9 +1,20 @@
 package level2ex4;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
 public class Main {
+    private static List<String> createStringsList(){
+        List<String> list = Arrays.asList("One", "125", "April", "event", "2540", "Clarinet", "99", "Year");
+        return list;
+    }
     public static void main(String[] args){
+        List<String> list;
+
+        list = createStringsList();
+
+        list.forEach(System.out::println);
         
     }
 }
