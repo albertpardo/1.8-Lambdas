928e5c4c14e5a13daf1e284765153cddc2a38b31
apm <apm@mail.com>
2025-11-27 11:09:15 +0100

    [T8L3E1] refactor using enums

--- src/level3ex1/Main.java
+++ src/level3ex1/Main.java
@@ -8,19 +8,38 @@ import java.util.Objects;
 
 public class Main {
 
+    private enum Course {
+        PHP("PHP"),
+        ANGULAR("ANGULAR"),
+        JAVA("JAVA"),
+        CPLUSPLUS("C++");
+
+        private final String name;
+
+        Course(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private static final int OF_LEGAL_AGE = 18;
+
     private static List<Student> getTenStudentList(){
         List<Student> students = new ArrayList<>();
 
-        students.add(new Student("Antonella Barber", 20, "PHP", 8.5));
-        students.add(new Student("Marco Martelli", 24, "ANGULAR" , 4.1));
-        students.add(new Student("Mireia Boch", 30, "JAVA", 9.9));
-        students.add(new Student("Francisco Perez", 46, "PHP", 9.1));
-        students.add((new Student("Judith Massana", 29, "C++", 9.8)));
-        students.add((new Student("Andreu Garcia", 32, "JAVA", 9.2)));
-        students.add((new Student("Jordi Baylina", 49, "PHP", 8.9)));
-        students.add((new Student("Sergio Ramirez", 22, "PHP", 7.5)));
-        students.add((new Student("Ruth Zamora", 16, "JAVA", 4.8)));
-        students.add((new Student("Pau Soret", 17, "JAVA", 9.4)));
+        students.add(new Student("Antonella Barber", 20, Course.PHP.getName(), 8.5));
+        students.add(new Student("Marco Martelli", 24, Course.ANGULAR.getName() , 4.1));
+        students.add(new Student("Mireia Boch", 30, Course.JAVA.getName(), 9.9));
+        students.add(new Student("Francisco Perez", 46, Course.PHP.getName(), 9.1));
+        students.add((new Student("Judith Massana", 29, Course.CPLUSPLUS.getName(), 9.8)));
+        students.add((new Student("Andreu Garcia", 32, Course.JAVA.getName(), 9.2)));
+        students.add((new Student("Jordi Baylina", 49, Course.PHP.getName(), 8.9)));
+        students.add((new Student("Sergio Ramirez", 22, Course.PHP.getName(), 7.5)));
+        students.add((new Student("Ruth Zamora", 16, Course.JAVA.getName(), 4.8)));
+        students.add((new Student("Pau Soret", 17, Course.JAVA.getName(), 9.4)));
         return students;
     }
 
@@ -51,14 +70,14 @@ public class Main {
     private static void filterAndPrintStudentsWithNoteGraterThanFiveAndNotPhp(List<Student> students){
         List<Student> filterStudents;
 
-        filterStudents = students.stream().filter(student -> ( student.getNote() > 5.0 && !Objects.equals(student.getCourse(), "PHP"))).toList();
+        filterStudents = students.stream().filter(student -> ( student.getNote() > 5.0 && !Objects.equals(student.getCourse(), Course.PHP.getName()))).toList();
         printFilteredStudents("--- Students with note > 5.0 and They are not in 'PHP' course:", filterStudents);
     }
 
     private static void filterAndPrintJavaStudentsOfLegalAge(List<Student> students){
         List<Student> filterStudents;
 
-        filterStudents = students.stream().filter(student -> ( Objects.equals(student.getCourse(), "JAVA") && student.getAge() >= 18 )).toList();
+        filterStudents = students.stream().filter(student -> ( Objects.equals(student.getCourse(), Course.JAVA.getName()) && student.getAge() >= OF_LEGAL_AGE )).toList();
         printFilteredStudents("--- Of Legal Age Students in 'JAVA' course:", filterStudents);
     }
 
